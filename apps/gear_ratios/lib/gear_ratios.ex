c4cd378ec5677df31bc9b1b4ea282bd1c39a2dd2
FranPek <pedreirafran@gmail.com>
2023-12-03 20:14:54 +0100

    Gear ratios #Day3 (Refactor)

--- apps/gear_ratios/lib/gear_ratios.ex
+++ apps/gear_ratios/lib/gear_ratios.ex
@@ -18,7 +18,7 @@ defmodule GearRatios do
       |> String.split("\r\n")
       |> Enum.map(&String.graphemes/1)
 
-    result = matrix
+    matrix
       |> Enum.with_index
       |> Enum.map(fn  ({line, index}) -> process_line(matrix, Enum.with_index(line), index) end)
       |> Enum.sum
@@ -82,30 +82,30 @@ defmodule GearRatios do
 
   """
   def gear_ratios_two(path) do
-    matrix = File.read!(path) |> String.split("\r\n") |> Enum.map(&String.graphemes/1)
-
-    result = matrix
-             |> Enum.with_index
-             |> Enum.map(fn  ({line, index}) -> process_line_two(matrix, Enum.with_index(line), index) end)
-    |> Enum.sum
+    matrix = File.read!(path)
+             |> String.split("\r\n")
+             |> Enum.map(&String.graphemes/1)
+    matrix
+      |> Enum.with_index
+      |> Enum.map(fn  ({line, index}) -> process_matrix_lines(matrix, Enum.with_index(line), index) end)
+      |> Enum.sum
   end
 
-  def process_line_two(_matrix, _line, _y, _parts \\ 0)
-  def process_line_two(matrix, [ {"*", x} | tl ], y, parts), do: process_line_two(matrix, tl, y, parts + gear_adjacent(matrix, x, y))
-  def process_line_two(matrix, [ {item, x} | tl ], y, parts), do: process_line_two(matrix, tl, y, parts)
-  def process_line_two(_matrix, _tl, _y, parts), do: parts
+  def process_matrix_lines(_matrix, _line, _y, _parts \\ 0)
+  def process_matrix_lines(matrix, [ {"*", x} | tl ], y, parts), do: process_matrix_lines(matrix, tl, y, parts + gear_ratio(matrix, x, y))
+  def process_matrix_lines(matrix, [ _ | tl ], y, parts), do: process_matrix_lines(matrix, tl, y, parts)
+  def process_matrix_lines(_matrix, _tl, _y, parts), do: parts
 
-  def gear_adjacent(matrix, x, y) do
-    # item is always *
+  def gear_ratio(matrix, x, y) do
     adjacent =
-      [get_number(matrix, x-1, y+0),
-      get_number(matrix, x-1, y+1),
-      get_number(matrix, x+0, y+1),
-      get_number(matrix, x+1, y+1),
-      get_number(matrix, x+1, y+0),
-      get_number(matrix, x+1, y-1),
-      get_number(matrix, x+0, y-1),
-      get_number(matrix, x-1, y-1)]
+      [get_number_in_matrix(matrix, x-1, y+0),
+      get_number_in_matrix(matrix, x-1, y+1),
+      get_number_in_matrix(matrix, x+0, y+1),
+      get_number_in_matrix(matrix, x+1, y+1),
+      get_number_in_matrix(matrix, x+1, y+0),
+      get_number_in_matrix(matrix, x+1, y-1),
+      get_number_in_matrix(matrix, x+0, y-1),
+      get_number_in_matrix(matrix, x-1, y-1)]
       |> Enum.uniq
       |> Enum.filter(fn x -> x != nil end)
       |> Enum.map(&String.to_integer/1)
@@ -114,34 +114,32 @@ defmodule GearRatios do
         length(adjacent) == 2 -> Enum.product(adjacent)
         true -> 0
       end
-
   end
 
-  def get_number(matrix, x, y) do
+  def get_number_in_matrix(matrix, x, y) do
     cond do
       matrix |> Enum.at(y) == nil or matrix |> Enum.at(y) |> Enum.at(x) == nil  -> nil
-      matrix |> Enum.at(y) |> Enum.at(x) |> Integer.parse != :error             -> get_through_line(matrix |> Enum.at(y), x-1, :left) <>
+      matrix |> Enum.at(y) |> Enum.at(x) |> Integer.parse != :error             -> get_number_in_line(matrix |> Enum.at(y), x-1, :left) <>
                                                                                      (matrix |> Enum.at(y) |> Enum.at(x)) <>
-                                                                                   get_through_line(matrix |> Enum.at(y), x+1, :right)
+                                                                                   get_number_in_line(matrix |> Enum.at(y), x+1, :right)
       true -> nil
     end
   end
 
-def get_through_line(line, index, direction, acc \\ "")
-def get_through_line(line, index, direction, acc) do
+  def get_number_in_line(line, index, direction, acc \\ "")
+  def get_number_in_line(line, index, :left, acc) do
     item = Enum.at(line, index)
     cond do
-      direction == :left -> cond do
-                              item != nil and item != "" and Integer.parse(item) != :error  -> get_through_line(line, index-1, :left, Enum.at(line, index) <> acc)
-                               true -> acc
-                            end
-      direction == :right -> cond do
-                               item != nil and item != "" and Integer.parse(item) != :error -> get_through_line(line, index+1, :right, acc <> Enum.at(line, index))
-                               true -> acc
-                             end
+      item != nil and item != "" and Integer.parse(item) != :error  -> get_number_in_line(line, index-1, :left, Enum.at(line, index) <> acc)
       true -> acc
     end
   end
-
+  def get_number_in_line(line, index, :right, acc) do
+    item = Enum.at(line, index)
+    cond do
+      item != nil and item != "" and Integer.parse(item) != :error -> get_number_in_line(line, index+1, :right, acc <> Enum.at(line, index))
+      true -> acc
+    end
+  end
 
 end
